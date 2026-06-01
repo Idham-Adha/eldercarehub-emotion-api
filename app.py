529bab8ae05cf6bb455322879089b1dc4b330069
Idham-Adha <wanmuhammadidhamadha@gmail.com>
2026-06-01 08:35:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from deepface import DeepFace
 import tempfile
 import os
 
@@ -14,11 +13,14 @@ def analyze():
 
     try:
 
+        # Import DeepFace hanya apabila endpoint dipanggil
+        from deepface import DeepFace
+
         if "image" not in request.files:
             return jsonify({
                 "status": "error",
                 "message": "No image uploaded"
-            })
+            }), 400
 
         image = request.files["image"]
 
@@ -37,7 +39,9 @@ def analyze():
 
         emotion = result[0]["dominant_emotion"]
 
-        os.remove(temp.name)
+        # Padam fail sementara
+        if os.path.exists(temp.name):
+            os.remove(temp.name)
 
         return jsonify({
             "status": "success",
@@ -49,10 +53,13 @@ def analyze():
         return jsonify({
             "status": "error",
             "message": str(e)
-        })
+        }), 500
+
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
         host="0.0.0.0",
-        port=5000
+        port=port
     )
